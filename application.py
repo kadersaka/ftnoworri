1407253822026354c6c2c4e8538f5c511136126b
kadersaka@gmail.com <VB5TUxzZA7t6U5d>
2021-10-03 08:39:42 +0100

    complet requirements

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Hello, World from kader!'
+    return 'Hello, World from kader added complet requirements!'
 
 if __name__ == '__main__':
     application.run(debug=False)
