030719d4e4b0b172ea885680d95b40f7d9ccf658
kadersaka@gmail.com <VB5TUxzZA7t6U5d>
2021-10-03 08:31:39 +0100

    git ignore added back

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hello, World from kader!'
 
 if __name__ == '__main__':
     application.run(debug=False)
